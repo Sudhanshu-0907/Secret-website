f8ab42e0a0ae1f9a414aef09ef19a39e463a792a
Sudhanshu Singh <sudhanshusingh0907@gmail.com>
2022-04-14 10:11:49 +0530

    Hashing level 3 password

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+// const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 const app = express();
 
@@ -27,7 +28,7 @@ const userSchema=new mongoose.Schema({
     password:String
 })
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET ,encrptedFields:['password']});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET ,encrptedFields:['password']});
 
 
 const User = new mongoose.model("User",userSchema);
@@ -50,7 +51,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(!err){
@@ -64,7 +65,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email: username},function(err,foundUser){
         if(err){
@@ -77,9 +78,6 @@ app.post("/login",function(req,res){
             
         }
     })
-    // else{
-        res.render("register");
-    // }
 });
 
 
